080e881fd8330196d174421755ec663da4d04c07
cohnor08 <cohnorwalsh@icloud.com>
2026-05-24 22:49:42 +1200

    fix: import from @firebase/auth directly to get RN build

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from '@firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -14,5 +15,15 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = getAuth(app);
+let auth;
+try {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} catch {
+  const { getAuth } = require('@firebase/auth');
+  auth = getAuth(app);
+}
+
+export { auth };
 export const db = getFirestore(app);
